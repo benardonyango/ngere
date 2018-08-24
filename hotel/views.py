8622b747ed6cb1c1f400b9409b706c7ed1ef7a2e
benardonyango <benardonyango16@gmail.com>
2018-08-24 13:57:01 +0300

    First commit

---
+++ hotel/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'index.html')
+
+def details(request):
+    return render(request, 'single.html')
